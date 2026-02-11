9da2234045a128858dfaa982b9268eac7b70e0ec
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-02-11 11:12:28 +0700

    feat: create for floating

--- main.go
+++ main.go
@@ -7,18 +7,19 @@ package main
 
 import "fmt"
 
-func keliling(phi int, r int) int {
-	result := 2 * phi * r
-	return result
+func keliling(phi float32, r int) int {
+	// result float32 := 2 * phi * r
+	return int(2 * phi * float32(r))
 }
 
 func luas(phi int, r int) int {
-	result := phi * r * r
-	return result
+	// result := phi * r * r
+	return int(phi * r * r)
 }
 func main() {
-	fmt.Println(keliling(3, 3))
+	fmt.Println(keliling(3.14, 3))
 	fmt.Println(luas(3, 3))
 
 	fmt.Println(3.14 * 3)
+
 }
